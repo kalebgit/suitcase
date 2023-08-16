46bacc91ce2a229f99e0501dfe0983ba6bfb34ec
Emiliano Jimenez <emilianokaleb@gmail.com>
2023-08-16 16:09:40 -0600

    chore():

--- src/components/InputBox/InputBox.jsx
+++ src/components/InputBox/InputBox.jsx
@@ -4,14 +4,14 @@
 import './InputBox.scss'
 
 
-function InputBox({type, labelText, options, boxClass, inputClass, labelClass}){
+function InputBox({type, labelText, options, boxClass, inputClass, labelClass, onChangeHandler}){
 
     return (
         <>
             {type == "plaintext" ? 
                 <div className={"relative pt-7 px-2 " + boxClass}>
                     <input type="text" className={" input-bar w-full py-1 outline-none " +
-                        "bg-transparent " +  inputClass} required/>
+                        "bg-transparent " +  inputClass} onChange={onChangeHandler} required/>
                     <label className={"label-bar absolute " + labelClass}>{labelText}</label>
                 </div> 
             : type == "date" ?
@@ -21,7 +21,7 @@ function InputBox({type, labelText, options, boxClass, inputClass, labelClass}){
                 </div> 
             : type == "number" ? 
                 <div className={"relative flex flex-row justify-start items-center gap-2 " + boxClass}>
-                    <input type="number" min="1" step="1" className={"order-2 w-24 p-2 " + inputClass} required/>
+                    <input type="number" min="1" step="1" className={"order-2 w-24 p-2 " + inputClass} onChange={onChangeHandler} required/>
                     <label className={" order-1 " + labelClass}>{labelText}</label>
                 </div>
             : type == "select" ?
--- src/routes/Test/Test.jsx
+++ src/routes/Test/Test.jsx
@@ -1,9 +1,28 @@
-
+import Form from '../../components/Form/Form'
+import InputBox from '../../components/InputBox/InputBox'
+import {useState} from 'react'
 
 function Test(){
+    const [inputText, setInputText] = useState("")
+
+    const changeHandler = (e)=>{
+        setInputText(e.target.value)
+    }
+
     return (
-        <>
-        </>
+        <main>
+            <Form formClass="bg-slate-200 p-4">
+                <InputBox type="plaintext" labelText="Ingresa lo que quieras" boxClass="bg-slate-400" 
+                    onChangeHandler ={changeHandler}/>
+            </Form>
+
+            {inputText.length > 0 ? 
+            <>
+                <h1>Escribiste: </h1>
+                <p>{inputText}</p>
+            </> : <></>}
+            
+        </main>
     )
 }
 
